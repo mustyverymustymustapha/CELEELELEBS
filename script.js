3601b6317cb5e8898441e3d85954e14d102c226d
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-06 14:53:39 +0000

    muauahahaha

---
+++ script.js
@@ -0,0 +1,7 @@
+const celebrityNames = [
+    "Leonardo DiCaprio",
+    "Emma Stone",
+    "Chris Hemsworth",
+    "Jennifer Lawrence",
+    "The Rock"];
+const generateBtn = document.getElementById("generate-btn");
