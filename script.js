5d521ba7f831756c02c4b76b222c4c08c58e37d8
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-06 17:31:20 +0000

    hehehe

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const celebrityNames = [
     "Jennifer Lawrence",
     "The Rock"];
 const generateBtn = document.getElementById("generate-btn");
+const celebrityNameDiv = document.getElementById("celeb-name"); generateBtn.addEventListener("click", () => { const randomArrItem = Math.floor(Math.random() * celebrityNames.length); const randomName = celebrityNames[randomArrItem]; celebrityNameDiv.innerText = randomName; });
